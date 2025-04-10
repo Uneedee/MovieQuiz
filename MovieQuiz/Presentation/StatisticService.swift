d2b7907be09d125a52080cbdc85fd549012dd135
Alexey <aratusha2602@gmail.com>
2025-04-10 14:16:47 +0300

    Реализован класс со статистикой результатов

--- MovieQuiz/Presentation/StatisticService.swift
+++ MovieQuiz/Presentation/StatisticService.swift
@@ -2,26 +2,36 @@ import UIKit
 
 class StatisticService: StatisticServiceProtocol {
     
+    private let storage: UserDefaults = .standard
+    
+    private enum Keys: String {
+        case correct = "correctAnswers"
+        case bestGameDate = "bestGameDate"
+        case gamesCount = "gamesCount"
+        case totalAnswers = "totalAnswers"
+    }
+    
     var gamesCount: Int {
-        get { return UserDefaults.standard.integer(forKey: "gamesCount") }
-        set { UserDefaults.standard.set(newValue, forKey: "gamesCount") } }
+        get { return storage.integer(forKey: Keys.gamesCount.rawValue) }
+        set { storage.set(newValue, forKey: Keys.gamesCount.rawValue) } }
     
     var bestGame: GameResult {
         get { return GameResult(
-            correct: UserDefaults.standard.integer(forKey: "correctAnswers"),
-            total: UserDefaults.standard.integer(forKey: "totalAnswers"),
-            date: UserDefaults.standard.object(forKey: "bestGamedate") as? Date ?? Date()) }
+            correct: storage.integer(forKey: Keys.correct.rawValue),
+            total: storage.integer(forKey: Keys.totalAnswers.rawValue),
+            date: storage.object(forKey: Keys.bestGameDate.rawValue) as? Date ?? Date()) }
         set {
-            UserDefaults.standard.set(newValue.correct, forKey: "correctAnswers")
-            UserDefaults.standard.set(newValue.total, forKey: "totalAnswers")
-            UserDefaults.standard.set(newValue.date, forKey: "bestGamedate")
+            storage.set(newValue.correct, forKey: Keys.correct.rawValue)
+            storage.set(newValue.total, forKey: Keys.totalAnswers.rawValue)
+            storage.set(newValue.date, forKey: Keys.bestGameDate.rawValue)
         }
     }
     var totalAccuracy: Double {
-        get { let correctAnswers = UserDefaults.standard.integer(forKey: "correctAnswers")
-            let gamesCount = UserDefaults.standard.integer(forKey: "gamesCount")
+        get { let correctAnswers = storage.integer(forKey: Keys.correct.rawValue)
+            let gamesCount = storage.integer(forKey: Keys.gamesCount.rawValue)
             if gamesCount  > 0 {
-                return (Double(correctAnswers)  / Double(gamesCount) * 10 ) * 100 }
+                return (Double(correctAnswers)  / (Double(gamesCount) * 10) ) * 100 }
+            else { return Double(gamesCount) }
         }
         
         func store(correct count: Int, total amount: Int) {
